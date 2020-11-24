4625f2342c2297b21ed941e62386ebd2396df296
vovantamvn <vovantam.dev@gmail.com>
2020-11-24 14:40:43 +0700

    Tmp

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -28,99 +28,99 @@ const char* vetifyLogin(const char* data);
 // Hàm login, sẽ được chạy trong thread
 DWORD WINAPI doLogin(LPVOID lpParam);
 
-int main(int argc, char** argv)
-{
-    // validate argument
-    if (argc != 2) {
-        printf("Please enter the port\n");
-        return 1;
-    }
-
-    // import data from file
-    users = readUsersFromFile();
-    if (users.empty())
-    {
-        printf("File is empty\n");
-        return 1;
-    }
-
-    printf("Server is running in port %s\n", argv[1]);
-
-    WSADATA wsaData;
-    int iResult;
-
-    SOCKET ListenSocket = INVALID_SOCKET;
-
-    struct addrinfo* result = NULL;
-    struct addrinfo hints;
-
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        printf("WSAStartup failed with error: %d\n", iResult);
-        return 1;
-    }
-
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_PASSIVE;
-
-    // Resolve the server address and port
-    iResult = getaddrinfo(NULL, argv[1], &hints, &result);
-    if (iResult != 0) {
-        printf("getaddrinfo failed with error: %d\n", iResult);
-        WSACleanup();
-        return 1;
-    }
-
-    // Create a SOCKET for connecting to server
-    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (ListenSocket == INVALID_SOCKET) {
-        printf("socket failed with error: %ld\n", WSAGetLastError());
-        freeaddrinfo(result);
-        WSACleanup();
-        return 1;
-    }
-
-    // Setup the TCP listening socket
-    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-    if (iResult == SOCKET_ERROR) {
-        printf("bind failed with error: %d\n", WSAGetLastError());
-        freeaddrinfo(result);
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    freeaddrinfo(result);
-
-    iResult = listen(ListenSocket, SOMAXCONN);
-    if (iResult == SOCKET_ERROR) {
-        printf("listen failed with error: %d\n", WSAGetLastError());
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    DWORD thread;
-    SOCKET clientSocket = INVALID_SOCKET;
-
-    while (true)
-    {
-        // Accept a client socket
-        clientSocket = accept(ListenSocket, NULL, NULL);
-
-        // Sau khi accept một client thì tạo một thread
-        CreateThread(NULL, 0, doLogin, (LPVOID)clientSocket, 0, &thread);
-    }
-
-    closesocket(ListenSocket);
-    WSACleanup();
-
-    return 0;
-}
+//int main(int argc, char** argv)
+//{
+//    // validate argument
+//    if (argc != 2) {
+//        printf("Please enter the port\n");
+//        return 1;
+//    }
+//
+//    // import data from file
+//    users = readUsersFromFile();
+//    if (users.empty())
+//    {
+//        printf("File is empty\n");
+//        return 1;
+//    }
+//
+//    printf("Server is running in port %s\n", argv[1]);
+//
+//    WSADATA wsaData;
+//    int iResult;
+//
+//    SOCKET ListenSocket = INVALID_SOCKET;
+//
+//    struct addrinfo* result = NULL;
+//    struct addrinfo hints;
+//
+//    // Initialize Winsock
+//    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+//    if (iResult != 0) {
+//        printf("WSAStartup failed with error: %d\n", iResult);
+//        return 1;
+//    }
+//
+//    ZeroMemory(&hints, sizeof(hints));
+//    hints.ai_family = AF_INET;
+//    hints.ai_socktype = SOCK_STREAM;
+//    hints.ai_protocol = IPPROTO_TCP;
+//    hints.ai_flags = AI_PASSIVE;
+//
+//    // Resolve the server address and port
+//    iResult = getaddrinfo(NULL, argv[1], &hints, &result);
+//    if (iResult != 0) {
+//        printf("getaddrinfo failed with error: %d\n", iResult);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Create a SOCKET for connecting to server
+//    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+//    if (ListenSocket == INVALID_SOCKET) {
+//        printf("socket failed with error: %ld\n", WSAGetLastError());
+//        freeaddrinfo(result);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Setup the TCP listening socket
+//    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+//    if (iResult == SOCKET_ERROR) {
+//        printf("bind failed with error: %d\n", WSAGetLastError());
+//        freeaddrinfo(result);
+//        closesocket(ListenSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    freeaddrinfo(result);
+//
+//    iResult = listen(ListenSocket, SOMAXCONN);
+//    if (iResult == SOCKET_ERROR) {
+//        printf("listen failed with error: %d\n", WSAGetLastError());
+//        closesocket(ListenSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    DWORD thread;
+//    SOCKET clientSocket = INVALID_SOCKET;
+//
+//    while (true)
+//    {
+//        // Accept a client socket
+//        clientSocket = accept(ListenSocket, NULL, NULL);
+//
+//        // Sau khi accept một client thì tạo một thread
+//        CreateThread(NULL, 0, doLogin, (LPVOID)clientSocket, 0, &thread);
+//    }
+//
+//    closesocket(ListenSocket);
+//    WSACleanup();
+//
+//    return 0;
+//}
 
 const char* vetifyLogin(const char* data) {
     const int LENGTH = 256;
